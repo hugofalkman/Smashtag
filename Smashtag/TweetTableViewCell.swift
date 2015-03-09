4eab80aa84838e87dd025a1b0ee403590bdf41ae
hugofalkman <zilla@Hs-MacBook-Pro.local>
2015-03-10 00:49:50 +0100

    Lecture 10 Demo almost complete

---
+++ Smashtag/TweetTableViewCell.swift
@@ -0,0 +1,60 @@
+//
+//  TweetTableViewCell.swift
+//  Smashtag
+//
+//  Created by H Hugo Falkman on 2015-03-09.
+//  Copyright (c) 2015 H Hugo Fakman. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+    
+    var tweet: Tweet? {
+        didSet {
+            updateUI()
+        }
+    }
+
+    @IBOutlet weak var tweetProfileImageView: UIImageView!
+    @IBOutlet weak var tweetScreenNameLabel: UILabel!
+    @IBOutlet weak var tweetTextLabel: UILabel!
+    @IBOutlet weak var tweetCreatedLabel: UILabel!
+    
+    func updateUI() {
+        // reset any existing tweet information
+        tweetTextLabel?.attributedText = nil
+        tweetScreenNameLabel?.text = nil
+        tweetProfileImageView?.image = nil
+        tweetCreatedLabel?.text = nil
+        
+        // load new information from our tweet (if any)
+        if let tweet = self.tweet
+        {
+            tweetTextLabel?.text = tweet.text
+            if tweetTextLabel?.text != nil  {
+                for _ in tweet.media {
+                    tweetTextLabel.text! += " 📷"
+                }
+            }
+            
+            tweetScreenNameLabel?.text = "\(tweet.user)" // tweet.user.description
+            
+            if let profileImageURL = tweet.user.profileImageURL {
+                if let imageData = NSData(contentsOfURL: profileImageURL) { // blocks main thread!
+                    tweetProfileImageView?.image = UIImage(data: imageData)
+                }
+            }
+            
+            let formatter = NSDateFormatter()
+            formatter.locale = NSLocale(localeIdentifier: "en_US")
+            if NSDate().timeIntervalSinceDate(tweet.created) > 24*60*60 {
+                formatter.dateStyle = NSDateFormatterStyle.ShortStyle
+            } else {
+                formatter.timeStyle = NSDateFormatterStyle.ShortStyle
+            }
+            tweetCreatedLabel?.text = formatter.stringFromDate(tweet.created)
+        }
+        
+    }
+}
--- Smashtag/TweetTableViewController.swift
+++ Smashtag/TweetTableViewController.swift
@@ -8,41 +8,110 @@
 
 import UIKit
 
-class TweetTableViewController: UITableViewController {
-
+class TweetTableViewController: UITableViewController, UITextFieldDelegate {
+    
+    var tweets = [[Tweet]]()
+    
+    var searchText: String? = "#stanford" {
+        didSet {
+            lastSuccessfulRequest = nil
+            searchTextField?.text = searchText
+            tweets.removeAll()
+            tableView.reloadData()
+            refresh()
+        }
+    }
+    
+    // MARK: - View Controller Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.estimatedRowHeight = tableView.rowHeight
+        tableView.rowHeight = UITableViewAutomaticDimension
+        refresh()
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
+    
+    var lastSuccessfulRequest: TwitterRequest?
+    
+    var nextRequestToAttempt: TwitterRequest? {
+        if lastSuccessfulRequest == nil {
+            if searchText != nil {
+                return TwitterRequest(search: searchText!, count: 100)
+            } else {
+                return nil
+            }
+        } else {
+            return lastSuccessfulRequest!.requestForNewer
+        }
+    }
 
-    // MARK: - Table view data source
+    func refresh() {
+        if refreshControl != nil {
+            refreshControl?.beginRefreshing()
+        }
+        refresh(refreshControl)
+    }
+    
+    @IBAction func refresh(sender: UIRefreshControl?) {
+        if searchText != nil {
+            if let request = nextRequestToAttempt {
+                request.fetchTweets { (newTweets) -> Void in
+                    dispatch_async(dispatch_get_main_queue()) { () -> Void in
+                        if newTweets.count > 0 {
+                            self.lastSuccessfulRequest = request
+                            self.tweets.insert(newTweets, atIndex: 0)
+                            self.tableView.reloadData()
+                        }
+                        sender?.endRefreshing()
+                    }
+                }
+            }
+        } else {
+            sender?.endRefreshing()
+        }
+    }
+    
+    @IBOutlet weak var searchTextField: UITextField! {
+        didSet {
+            searchTextField.delegate = self
+            searchTextField.text = searchText
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if textField == searchTextField {
+            textField.resignFirstResponder()
+            searchText = textField.text
+        }
+        return true
+    }
+    
+    // MARK: - UITableViewDataSource
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Potentially incomplete method implementation.
-        // Return the number of sections.
-        return 0
+        return tweets.count
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete method implementation.
-        // Return the number of rows in the section.
-        return 0
+        return tweets[section].count
     }
 
-    /*
+    private struct Storyboard {
+        static let cellReuseIdentifier = "Tweet"
+    }
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
+        let cell = tableView.dequeueReusableCellWithIdentifier(Storyboard.cellReuseIdentifier, forIndexPath: indexPath) as TweetTableViewCell
 
         // Configure the cell...
-
+        cell.tweet = tweets[indexPath.section][indexPath.row]
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
