a7de1197e6bfa5d3ad85e9987ad5d15accfe9180
hugofalkman <zilla@Hs-MacBook-Pro.local>
2015-04-05 16:09:56 +0200

    Added app and other icons

---
+++ Smashtag/SettingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.swift
+//  Smashtag
+//
+//  Created by H Hugo Falkman on 2015-04-05.
+//  Copyright (c) 2015 H Hugo Fakman. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
